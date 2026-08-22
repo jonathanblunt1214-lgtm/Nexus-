41dab5bc318b03681018741854bf9f09d1591d58
Jon <jonathanblunt1214@gmail.com>
2026-08-22 10:29:10 -0400

    Section 8: register plugin platform IPC

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,8 @@
 // Nexus bootstrap: registers narrowly scoped upgrade IPC before loading the legacy main process.
 const { ipcMain, webContents } = require('electron');
 const { registerSection7Ipc } = require('./section7Ipc');
+const { registerSection8Ipc } = require('./section8Ipc');
 
 registerSection7Ipc({ ipcMain, webContents });
+registerSection8Ipc({ ipcMain });
 require('./main');
