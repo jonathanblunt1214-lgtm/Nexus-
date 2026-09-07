08c06fb8a8e24002320c80fda109c5b7fc0307ca
Jon <jonathanblunt1214@gmail.com>
2026-09-06 20:57:15 -0400

    Keep generated path validation internal

--- pureLogic.js
+++ pureLogic.js
@@ -103,7 +103,6 @@ function parseJestStyleResults(jsonText) {
 
 module.exports = {
   sanitizeProjectFolderName,
-  isSafeGeneratedProjectPath,
   parseGeneratedFiles,
   detectStartCommand,
   escapeRegex,
